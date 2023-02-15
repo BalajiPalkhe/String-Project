9d7b1c2892e85f100e2bb573fe20645cd88ea0f2
Balaji <bpalkhe@gmail.com>
2023-02-15 12:55:40 +0530

    Added myEndsWithMethod into new repo

--- StringProblemsImplements.java
+++ StringProblemsImplements.java
@@ -433,6 +433,32 @@ public class StringProblemsImplements implements StringProblemsInterface
 		
 	}
 
+
+
+	public void myEndsWithMethod(String s1, String s2) 
+	{
+		
+		boolean result=myEndsWith(s1,s2);
+		System.out.println(result);
+	}
+	private static boolean myEndsWith(String s1, String s2) 
+	{
+		if(s2.length()>s1.length())
+			return false;
+		
+		int i=s1.length()-1;
+		int j=s2.length()-1;
+		
+		while(j>=0)
+		{
+			if(s1.charAt(i)!=s2.charAt(j))
+				return false;
+			i--;
+			j--;
+		}
+		return true;
+	}
+
 	
 
 	
