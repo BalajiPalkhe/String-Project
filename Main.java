74093c82bdab6f409549703f52fc2cfa90438f2a
Balaji <bpalkhe@gmail.com>
2023-02-16 11:08:39 +0530

    methods calling added to main number 3 and 4

--- Main.java
+++ Main.java
@@ -17,7 +17,7 @@ public class Main {
 		
 		while(true)
 		{
-			System.out.println("-----------Choose option : ------------\n1.Conversion of String elements into character Array.\n2.Get the character at given index.");
+			System.out.println("-----------Choose option : ------------\n1.Conversion of String elements into character Array.\n2.Get the character at given index.\n3.get index of all characters in Given String.\n4.Repeat each character twice in given String.");
 			int input=scan.nextInt();
 			
 			if(input==1)
@@ -29,6 +29,18 @@ public class Main {
 				System.out.println("Enter the index to get character : ");
 				int index=scan.nextInt();
 				s.getCharAtIndex(s1, index);
+				
+			}
+			else if(input==3)
+			{
+				s.getAllIndex(s1);
+			}
+			else if(input==4)
+			{
+				s.repeatEachCharTwice(s1);
+			}
+			else if(input==5)
+			{
 				
 			}
 			else if(input==0)
